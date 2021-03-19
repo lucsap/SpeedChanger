7bcc5974854e8e3cd73514ea0cb9a2528943018d
Lucas Pereira <luquinhabat@gmail.com>
2021-03-18 23:44:11 -0300

    Atualização da classe

--- SpeedChanger/content.js
+++ SpeedChanger/content.js
@@ -1,9 +1,8 @@
 const interval = setInterval(()=>{
-    const header = document.querySelector("._2O84H")
+    const header = document.querySelector("._1R3Un")
     if(header){
     console.log(header)
         clearInterval(interval)
-
 
         const button = document.createElement("button")
         button.innerHTML = "2x"
